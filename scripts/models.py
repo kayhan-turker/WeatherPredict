01df21e4ba131797526a64411c462644b9f43468
kayhan.turker <kturker92@gmail.com>
2025-02-18 15:30:35 -0500

    Model 032r: balance gen and disc layer widths and lr.

--- scripts/models.py
+++ scripts/models.py
@@ -77,10 +77,10 @@ class FakeImageGenerator(nn.Module):
 class LabelPredictor(nn.Module):
     def __init__(self, output_size):
         super(LabelPredictor, self).__init__()
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
-        self.conv3 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
-        self.fc1 = nn.Linear(64 * 16 * 32, 128)
+        self.conv1 = nn.Conv2d(3, 8, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(8, 16, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
+        self.fc1 = nn.Linear(32 * 16 * 32, 128)
         self.fc2 = nn.Linear(128, output_size + 1)  # Updated to match new label size
 
         self.pool = nn.MaxPool2d(2, 2)
