37012c3fc300064bae59809dc5f71a5f873641c1
kayhan.turker <kturker92@gmail.com>
2025-02-20 00:05:48 -0500

    Model 039b: remove scaling data (done before hand).

--- scripts/trainModel.py
+++ scripts/trainModel.py
@@ -112,7 +112,7 @@ class ImageDataset(Dataset):
 # 3. Transformations
 # ====================================
 transform = transforms.Compose([
-    transforms.Resize((IMAGE_HEIGHT, IMAGE_WIDTH)),
+    # transforms.Resize((IMAGE_HEIGHT, IMAGE_WIDTH)),
     transforms.ToTensor(),
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 ])
